02ca5b69d54435c84234ba18a0afeb23818e1023
Facundo Ortiz <fortiz@matechstudios.com>
2022-08-02 17:32:22 -0300

    rc=0 - mvp

--- main.py
+++ main.py
@@ -12,15 +12,15 @@ import random
 # simplified list for daily samples
 daily_temps = []  #[0.0 for d in range (31)]
 
+
 def randomTemps(daily_temps):
-    # del daily_temps[:]
     if len(daily_temps) >= 31:
         daily_temps.clear()
     for i in range(31):
         n = round(random.uniform(-1.5,24.0),1)
         daily_temps.insert(i,n)
-    tempList = daily_temps
-    return tempList
+    return daily_temps
+
 
 def showTemps(daily_temps):
     print("The number of samples is:", len(daily_temps))
@@ -29,14 +29,14 @@ def showTemps(daily_temps):
         print("day", index, "---> temperature:", value)
     return
 
-# def changeTemp():
-#     changeTemp=input("Do you want to change any record?\n 1 - Yes \n 2 - No")
-#     if changeTemp == 1 or changeTemp == "Yes":
-#         changeTempDay = int(input("Input day number:\n"))
-#         changeTempValue = float(input("Provide new value [float]:\n")
-#         daily_temps[0] = changeTempValue
-#     # listDays = [(i+1, daily_temps[i]) for i in range(len(daily_temps))]
-#     # print("day"+listDays[0]+1)
+
+def changeTemp():
+    changeTemp = input("Do you want to change any record?\n 1 - Yes \n 2 - No\n")
+    if changeTemp == "1" or changeTemp == "Yes":
+        changeTempDay = int(input("Input day number:\n"))
+        changeTempValue = float(input("Provide new value [float]:\n"))
+        daily_temps[changeTempDay-1] = changeTempValue
+
 
 def freshTemp(daily_temps):
     freshDaysCount = 0
@@ -47,17 +47,12 @@ def freshTemp(daily_temps):
             freshDaysCount += 1
     return freshDays
 
-    # for index, value in enumerate(daily_temps):
-    #     if value < 20:
-    #         chillDays2.append(daily_temps[index][value])
-    #     # print("day", int(str(index))+1, "---> temperature:", value)
-    # return chillDays2
 
 def maxTemp(daily_temps):
     # Assume first number in list is largest initially and assign it to variable "max"
     max = daily_temps[0]
-    # Now traverse through the list and compare each number with "max" value. Whichever is
-    # largest assign that value to "max'.
+    # Traverse through the list and compare each number with "max" value. Whichever is
+    # largest assign that value to "max'
     for x in daily_temps:
         if x > max:
             max = x
@@ -72,29 +67,33 @@ def minTemp(daily_temps):
 def banner():
     print(
         """
-        +==========================================+
-        | Python functions to analyze temperatures |
-        |                                          |
-        | Select an operation:                     |
-        | 1 - Generate temperature dataset         |
-        | 2 - Read number of samples taken         |
-        | 3 - Find "Highest" and "Lowest" temps    |
-        | 4 - Return a list of temps below 20ºC    |
-        | 0 - Exit this program                    |
-        +==========================================+
+        +===============================================+
+        | Python functions to analyze temperatures      |
+        |                                               |
+        | Select an operation:                          |
+        | 1 - Generate temperature dataset              |
+        | 2 - Show and count samples taken              |
+        | 3 - Find "Highest" and "Lowest" temps         |
+        | 4 - Return a list of temps below 20ºC         |
+        | 5 - Manually enter a new record (day, value)  |
+        | 9 - Display "Main Menu"                       |
+        | 0 - Exit this program                         |
+        +===============================================+
         """)
     print()
+
+def mainMenu():
     menu= int(input("Select an operation:\n"))
     
     while menu != 0:
         
         if menu == 1:
-            tempList = randomTemps(daily_temps)
-            print("created fake entries for July", tempList)
+            # Add random records to dataset/list
+            print("New records have been added to July dataset\n", randomTemps(daily_temps))
         
         elif menu == 2:
-            showTemps(tempList)
-            #print("Se tomaron Mediciones de temperatura ")
+            # Show temps and counts
+            showTemps(daily_temps)
 
         elif menu == 3:
             # Highest
@@ -103,16 +102,28 @@ def banner():
             print("Lowest temperature recorded for July:", minTemp(daily_temps))
 
         elif menu == 4:
-            # Chilly-Fresh
-            print("Temperatures below 20.0º:\n", freshTemp(daily_temps))
+            # Chilly-Fresh days
+            print("Temperatures below 20.0º -- Format like '[day, temp]'\n", freshTemp(daily_temps))
+
+        elif menu == 5:
+            # In case you want to get something changed/corrected (manually)
+            changeTemp()
+
+        elif menu == 9:
+            # Menu
+            banner()
 
         else:
             print(
                 """
                 +===================================+
-                | Try again using some valid option | 
+                | Try again using some valid option |
+                |                                   | 
+                |      Tip: Hit 9 to see Main Menu  | 
+                |           or 0 to Exit            | 
                 +===================================+
                 """)
         menu= int(input("Select an operation:\n"))
 
 banner()
+mainMenu()
