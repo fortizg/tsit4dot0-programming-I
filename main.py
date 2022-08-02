38bec347373e8ae4b992eac00e0bd62ad46a25d6
Facundo Ortiz <fortiz@matechstudios.com>
2022-08-02 11:51:11 -0300

    fcTemps drafted

--- main.py
+++ main.py
@@ -9,7 +9,7 @@
 import random
 
 # simplified list for daily samples
-daily_temps = [0.0 for d in range (31)]
+daily_temps = []  #[0.0 for d in range (31)]
 
 """ - Generar aleatoriamente las temperaturas medias registradas en el último mes de julio del año 2022.
 - Conocer cantidad de temperaturas generadas y dar la posibilidad de ingresar una manualmente.
@@ -18,23 +18,41 @@ daily_temps = [0.0 for d in range (31)]
 - Dar la posibilidad de salir del programa en cualquier momento.
  """
 
+print(len(daily_temps))
+
 def randomTemps(daily_temps):
-    for i in range(31):
-        n = random.randint(-1,24)
-        daily_temps.insert(i,n)
+    # del daily_temps[:]
+    if len(daily_temps) >= 31:
+        del daily_temps[:]
+    else:
+        for i in range(31):
+            n = round(random.uniform(-1.5,24.0),1)
+            daily_temps.insert(i,n)
+    return daily_temps
 
-randomTemps(daily_temps)
-
-# temperatures: min, max and avg
-highest_temp: 100.0
-lowest_temp: -20
+def showTemps(daily_temps):
+    # using enumerate to get index and value
+    # print ("List index-value are : ")
+    for index, value in enumerate(daily_temps):
+        print("day", int(str(index))+1, "---> temperature:", value)
+    return
+    # listDays = [(i+1, daily_temps[i]) for i in range(len(daily_temps))]
+    # print("day"+listDays[0]+1)
 
 def chillTemp(daily_temps):
-    chillDays = 0
+    chillDaysCount = 0
+    chillDays = []
+    chillDays2 = []
     for day in daily_temps:
         if day < 20.0:
-            chillDays += 1
+            chillDaysCount += 1
+            chillDays.append(day)
     return chillDays
+    # for index, value in enumerate(daily_temps):
+    #     if value < 20:
+    #         chillDays2.append(daily_temps[index][value])
+    #     # print("day", int(str(index))+1, "---> temperature:", value)
+    # return chillDays2
 
 def maxTemp(daily_temps):
     # Assume first number in list is largest initially and assign it to variable "max"
@@ -52,10 +70,15 @@ def minTemp(daily_temps):
     min_temps.sort()
     return min_temps[0]
 
+# populate random temperatures
+randomTemps(daily_temps)
 print(daily_temps)
+showTemps(daily_temps)
+
+
 # Highest
 print("Highest temperature was:", maxTemp(daily_temps))
 # Lowest
 print("Lowest temperature was:", minTemp(daily_temps))
 # Chill
-print("Number of days with temp below 20.0º:", chillTemp(daily_temps))
+print("Temperatures below 20.0º:\n", chillTemp(daily_temps))
